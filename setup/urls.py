87c7da70b73aa5444bd1fd98da87e3449d79612b
Coutrinlu20 <amarallua20@gmail.com.br>
2024-11-24 16:00:30 -0300

    removido o arquivo do bootstrap-5

--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
+
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuario', include('usuario.urls'))
+    path('', include('usuario.urls')),
 ]
--- usuario/urls.py
+++ usuario/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from usuario.views import home
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
 ]
