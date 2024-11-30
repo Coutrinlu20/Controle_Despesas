5dcc19fbd787084d2687555d384e2ea3c8ad6dc9
Coutrinlu20 <amarallua20@gmail.com.br>
2024-11-30 18:44:20 -0300

    atualizado

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Usuario(models.Model):
+    nome_user = models.CharField(max_length=150, unique=True)
+    email_user = models.EmailField(unique=True)
+    senha_user =  models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)# esta_ativo
+    created_at = models.DateTimeField(auto_now_add=True)# Criado_em
+
+
+    def __str__(self):
+        return f"Usuario [nome_user={self.nome_user}]"
